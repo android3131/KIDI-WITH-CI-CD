c1944cd71fb7c1a0648d709ec3ba95510cf4ae5a
android3131 <orofra31@gmail.com>
2021-09-08 04:56:47 -0400

    make it better

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authorizeRequests().antMatchers("/authenticate", 
 									"/getAllCategories","/getCoursesOfCategory/{a}",
 									"/getSpacesCourses", "/getAnimalsCourses","/getAnimalsCourses",
-									"/createkid", "/getArtsCourses").permitAll().
+									"/registerFirstKID", "/getArtsCourses").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
