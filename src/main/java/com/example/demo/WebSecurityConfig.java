ff777e225c6f3e252a8d8cbe2ec0432b4be1c1a4
android3131 <orofra31@gmail.com>
2021-09-08 03:58:25 -0400

    make it better

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -53,8 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
-				authorizeRequests().antMatchers("/getAllCategories").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/getAllCategories").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
