ce7801e3bf4961b33cbbd0aeb7a0e0d64d55690d
android3131 <orofra31@gmail.com>
2021-09-09 03:40:10 -0400

    make it better

--- src/main/java/com/example/demo/KidRepository.java
+++ src/main/java/com/example/demo/KidRepository.java
@@ -392,6 +392,26 @@ public List<String> getMeetingsByKidId(String kidId){
 	}
 	return null;	
 }
+
+public TreeMap<Integer,Integer> getKidsCategoryMonth(String categType){
+	HashMap<Integer, Integer> kidsCountByCategoryMonth = new HashMap<Integer,Integer>();
+	for(int i=0;i<12;i++) {
+		kidsCountByCategoryMonth.put(i, 0);
+	}
+	List<Kid> allKids = kidRepo.findAll();
+	for(int i=0;i<60;i++){
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(allKids.get(i).getActiveDate());
+		int num_kids = kidsCountByCategoryMonth.get(calendar.get(Calendar.MONTH));
+		String coursecateg = courseRepo.getASpecificCourse(allKids.get(i).getActiveCourses().get(0)).getCategoryId();
+		if(coursecateg.indexOf(categType)==0){
+			num_kids++;
+		}
+		kidsCountByCategoryMonth.put((calendar.get(Calendar.MONTH)), num_kids);			
+	}
+}
+
+
 /**
  * delete meeting from kid
  * @param kidId
@@ -410,21 +430,6 @@ public boolean deleteMeetingFromKid(String kidId, String meetingId) {
 }
 
 
-public TreeMap<Integer,Integer> getKidsCategoryMonth(String categType){
-	HashMap<Integer, Integer> kidsCountByCategoryMonth = new HashMap<Integer,Integer>();
-	for(int i=0;i<12;i++) {
-		kidsCountByCategoryMonth.put(i, 0);
-	}
-	List<Kid> allKids = kidRepo.findAll();
-	for(int i=0;i<60;i++){
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(allKids.get(i).getActiveDate());
-		int num_kids = kidsCountByCategoryMonth.get(calendar.get(Calendar.MONTH));
-		String coursecateg = courseRepo.getASpecificCourse(allKids.get(i).getActiveCourses().get(0)).getCategoryId();
-		if(coursecateg.indexOf(categType)==0){
-			num_kids++;
-		}
-		kidsCountByCategoryMonth.put((calendar.get(Calendar.MONTH)), num_kids);			
-	}
+
 
 }
